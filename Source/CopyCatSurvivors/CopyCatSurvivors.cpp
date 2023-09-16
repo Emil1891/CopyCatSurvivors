b0fe660b983ec2448c91944ebb71033ce1682435
Emil1891 <emillund2002@gmail.com>
2023-09-16 12:13:30 +0200

    init

---
+++ Source/CopyCatSurvivors/CopyCatSurvivors.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CopyCatSurvivors.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CopyCatSurvivors, "CopyCatSurvivors" );
+
+DEFINE_LOG_CATEGORY(LogCopyCatSurvivors)
+ 
