572a129ef646aa7288abe1137048a9fcd288d622
Lethargon <jonathan@eldfluga.se>
2023-09-17 22:06:44 +0200

    Cat struct and constructors.

---
+++ Source/CopyCatSurvivors/Cat.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Cat.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+// Sets default values
+ACat::ACat()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	BlackboardComponent = CreateDefaultSubobject<UBlackboardComponent>(TEXT("Blackboard"));
+}
+
+ACat::ACat(const FCatStruct& CatStruct)
+{
+	Properties = CatStruct;
+	ACat();
+}
+
+// Called when the game starts or when spawned
+void ACat::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACat::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ACat::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/CopyCatSurvivors/Cat.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Cat.generated.h"
+
+class UBlackboardComponent;
+
+USTRUCT()
+struct FCatStruct
+{
+	GENERATED_BODY()
+	FCatStruct()
+	{
+		
+	}
+	int NumClaws = 5;
+	int NumLegs = 4;
+	int NumTails = 1;
+	int NumEyes = 2;
+	int NumHearts = 1;
+	int Chonkiness = 1;
+	int BrainSize = 100;
+	int LungCapacity = 100;
+	bool BoneLess = false;
+	
+};
+
+UCLASS()
+class COPYCATSURVIVORS_API ACat : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACat();
+	ACat(const FCatStruct& CatStruct);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	UPROPERTY(EditAnywhere)
+	FCatStruct Properties;
+
+	UPROPERTY(EditAnywhere)
+	UBlackboardComponent* BlackboardComponent = nullptr;
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
