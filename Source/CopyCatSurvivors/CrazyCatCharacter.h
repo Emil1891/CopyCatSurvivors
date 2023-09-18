c35d5281645080484405e4eb690e8f6d141f94f5
Isabel <isabel.d.johansson@gmail.com>
2023-09-18 18:29:02 +0200

    Player dash + laser pointer + mechanical properties done

--- Source/CopyCatSurvivors/CrazyCatCharacter.h
+++ Source/CopyCatSurvivors/CrazyCatCharacter.h
@@ -13,5 +13,43 @@ UCLASS()
 class COPYCATSURVIVORS_API ACrazyCatCharacter : public ACopyCatSurvivorsCharacter
 {
 	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+
+	UFUNCTION(BlueprintGetter)
+	FColor GetLaserPointerColor() const  { return LaserPointerColor; }
+
+	UFUNCTION(BlueprintGetter)
+	float GetDashCoolDown() const  { return DashCooldown; }
+
+private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=MechanicalProperties, meta=(AllowPrivateAccess = "true"))
+	float MoveSpeed = 30.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=MechanicalProperties, meta=(AllowPrivateAccess = "true"))
+	float InitialHealth = 30.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=MechanicalProperties, meta=(AllowPrivateAccess = "true"))
+	float CurrentHealth = 30.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=MechanicalProperties, meta=(AllowPrivateAccess = "true"))
+	float LaserPointerStrength = 30.f;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=MechanicalProperties, meta=(AllowPrivateAccess = "true"))
+	float DashCooldown = 3.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=MechanicalProperties, meta=(AllowPrivateAccess = "true"))
+	int ExtraLives = 3.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=MechanicalProperties, meta=(AllowPrivateAccess = "true"))
+	int NumberOfRerolls= 30.f;
+
+	// Outfit??
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category=MechanicalProperties, meta=(AllowPrivateAccess = "true"))
+	FColor LaserPointerColor = FColor::Red;
 	
 };
