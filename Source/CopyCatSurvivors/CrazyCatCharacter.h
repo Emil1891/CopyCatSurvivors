92ffc035256097af60e8e577a27a261004c7f502
Isabel <isabel.d.johansson@gmail.com>
2023-09-18 12:51:50 +0200

    CharacterController/Player done for now

---
+++ Source/CopyCatSurvivors/CrazyCatCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CopyCatSurvivorsCharacter.h"
+#include "CrazyCatCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COPYCATSURVIVORS_API ACrazyCatCharacter : public ACopyCatSurvivorsCharacter
+{
+	GENERATED_BODY()
+	
+};
